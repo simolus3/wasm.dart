7681ed5ca5afb75af71ac88a71380b03ba8be625
Simon Binder <oss@simonbinder.eu>
2026-08-16 12:05:46 +0200

    Use link hooks to determine world

--- pkg/wasi/hook/link.dart
+++ pkg/wasi/hook/link.dart
@@ -8,12 +8,32 @@ void main(List<String> args) => link(args, (input, output) async {
   if (input.config.buildWasmComponent) {
     output.dependencies.add(Uri.parse('hook/wasm_abi.json'));
 
-    output.assets.webAssemblyComponents.add(
-      WasmComponentAsset(
-        encoded: json.decode(
-          File('lib/src/components/wasi_cli_command.json').readAsStringSync(),
-        ) as Map<String, Object?>,
-      ),
-    );
+    final uses = input.recordedUses!;
+    final usedWorlds = <String>{};
+
+    for (final MapEntry(key: def, value: calls) in uses.calls.entries) {
+      if (calls.isEmpty) continue;
+
+      if (def.name == 'commandComponent' &&
+          def.library.uri ==
+              'package:wasi/src/components/wasi_cli_command.dart') {
+        usedWorlds.add('lib/src/components/wasi_cli_command.json');
+      }
+      if (def.name == 'serviceComponent' &&
+          def.library.uri ==
+              'package:wasi/src/components/wasi_http_service.dart') {
+        usedWorlds.add('lib/src/components/wasi_http_service.json');
+      }
+    }
+
+    for (final used in usedWorlds) {
+      output.assets.webAssemblyComponents.add(
+        WasmComponentAsset(
+          encoded: json.decode(
+            File(used).readAsStringSync(),
+          ) as Map<String, Object?>,
+        ),
+      );
+    }
   }
 });
